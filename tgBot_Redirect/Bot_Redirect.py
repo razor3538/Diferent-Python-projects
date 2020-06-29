82526d4c54c883dc59b3509dedebebb4cf6a0354
razor3538 <razor3538@mail.ru>
2020-06-29 17:20:07 +0300

    A little changes for bot

--- tgBot_Redirect/Bot_Redirect.py
+++ tgBot_Redirect/Bot_Redirect.py
@@ -6,19 +6,33 @@ token = "1313409276:AAF0fiE27nNGNgD1tG5KlD5wlLRYQlXTYHA"
 bot = telebot.TeleBot(token)
 
 
+@bot.message_handler(commands=['start'])
+def hello(message):
+	bot.send_message(message.chat.id, "Hello, please insert a URL, which you wnat to check")
+
 @bot.message_handler(content_types=['text'])
 def redirect(message):
 	try:
 		r = requests.get(message.text)
 		if r.history:
-			bot.send_message(message.chat.id, 'Request was redirected')
+			mass = []
 			for resp in r.history:
-				bot.send_message(message.chat.id, 'Status code: ' + str(resp.status_code) + '\n' + resp.url)
-			bot.send_message(message.chat.id, 'Final destination: \n' + 'Status code: ' + str(r.status_code) + '\n' + r.url)
+				mass.append(resp.url)
+			text = ""
+			count = 0
+			while count < len(mass):
+				if count + 1 < len(mass):
+					text += mass[count] + '\n And then to:'
+				else:
+					text += mass[count]
+				count += 1
+
+			bot.send_message(message.chat.id, 'Request was redirected to: \n %s' %(text) + "\n Final destination: " + r.url)
 		else:
 			bot.send_message(message.chat.id, 'Request was not redirected')
 	except requests.exceptions.MissingSchema:
-		bot.send_message(message.chat.id, 'Invalid URL or URL is not found: \"%s\" \n Please insert corect URL' % (message.text))
-
+			bot.send_message(message.chat.id, 'Invalid URL or URL is not found: \"%s\" \n Please insert corect URL' % (message.text))
+	except requests.exceptions.InvalidSchema:
+			bot.send_message(message.chat.id, 'Invalid URL or URL is not found: \"%s\" \n Please insert corect URL' % (message.text))
 
 bot.polling()
